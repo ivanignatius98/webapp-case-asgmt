055623ae26833ff2d4749a35d57c6c57f0ef41b7
ivanignatius <ivanignatius98@gmail.com>
2023-01-25 15:30:15 +0700

    fix cover not saving

--- src/pages/api/books/store.js
+++ src/pages/api/books/store.js
@@ -4,7 +4,7 @@ import { post } from "@/services/api";
 const baseUrl = "https://gclone-api-staging.up.railway.app/api/v1/books"
 export default async function handler(req, res) {
   try {
-    const { _id, cover_data, ...payload } = req.body
+    const { _id, ...payload } = req.body
     const { token } = req.headers
 
     const endpoint = _id ? `${_id}/update` : "create"
